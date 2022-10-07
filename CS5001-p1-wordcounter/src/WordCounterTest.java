5b15ac614df204444a823f6284f65fa99eccd2cc
kiranbaby14 <kiranbaby256@gmail.com>
2022-10-07 14:34:12 +0100

    add unit test

--- CS5001-p1-wordcounter/src/WordCounterTest.java
+++ CS5001-p1-wordcounter/src/WordCounterTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,13 +11,13 @@ class WordCounterTest {
     @Test
     void arrayCountShouldReturnHundred() {
         WordCounter wordCounterObj =new WordCounter();
-        assertEquals(new int[]{100}, wordCounterObj.counter(1, "a-tale-of-two-cities.txt", new String[]{"Charles"}));
+        assertEquals(Arrays.toString(new int[]{100}), wordCounterObj.counter(1, "a-tale-of-two-cities.txt", new String[]{"Charles"}));
     }
 
     @Test
     void maxElementInArrayShouldReturnLongest() {
         WordCounter wordCounterObj = new WordCounter();
-        assertEquals("LONGEST", wordCounterObj.maxElementInArray(
+        assertEquals("Longest", wordCounterObj.maxElementInArray(
                 new ArrayList<String>(List.of(
                         "Word", "Longest", "Small"
                 ))));
