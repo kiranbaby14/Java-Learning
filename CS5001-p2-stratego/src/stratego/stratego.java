fb904bccf3f87c2b85ad54f9ec040539341c60e9
kiranbaby14 <kiranbaby256@gmail.com>
2022-10-30 07:05:13 +0000

    updates

--- CS5001-p2-stratego/src/stratego/stratego.java
+++ CS5001-p2-stratego/src/stratego/stratego.java
@@ -1,6 +1,8 @@
 package stratego;
 package stratego.pieces;
 
+import java.util.ArrayList;
+
 // enum CombatResult
 public final enum CombatResult {
     WIN,
@@ -10,10 +12,14 @@ public final enum CombatResult {
 
 // Class Game
 public class Game {
-    public final int HEIGHT = 10;
-    public final int WIDTH = 10;
-    public final int[] WATER_ROWS = {4, 5};
-    public final int[] WATER_COLS = {2, 3, 6, 7};
+    private final int HEIGHT = 10;
+    private final int WIDTH = 10;
+    private final int[] WATER_ROWS = {4, 5};
+    private final int[] WATER_COLS = {2, 3, 6, 7};
+
+    // Board of the game
+    public ArrayList<ArrayList<Integer>> game = new ArrayList<ArrayList<Integer>>();
+
 
     public Game(Player p0, Player p1) {
         this.p0 = p0;
@@ -22,27 +28,36 @@ public class Game {
 
     public Player getPlayer(int playerNumber) {
         if (this.p0.playernumber.equals(playerNumber)) {
-            return this.p0
-        } else if (!this.p0.playernumber.equals(playerNumber) && !this.p1.playernumber.equals(playerNumber) {
+            return this.p0;
+        } else if (!this.p0.playernumber.equals(playerNumber) && !this.p1.playernumber.equals(playerNumber)) {
             throw new IllegalArgumentException("No such player exists!!");
         }
 
-        return this.p1
+        return this.p1;
     }
 
     public Player getWinner() {
-        if(this.p0.lost) {
-            return this.p1
-        } else if(this.p1.lost) {
-            return this.p0
+        if (this.p0.lost) {
+            return this.p1;
+        } else if (this.p1.lost) {
+            return this.p0;
         }
     }
 
     public Square getSquare(int row, int col) {
-        if(row > 10 || col >10) {
+        if (row > HEIGHT || col > WIDTH) {
             throw new IndexOutOfBoundsException("Index is out of bounds");
         } else {
 
+            for (int i = 0; i < HEIGHT; i++) {
+                for (int j=0; j<WIDTH; j++) {
+                    if(row.equals(i) && col.equals(j)) {
+                        Square squareObj = new Square(game, HEIGHT, WIDTH, true);
+                    } else {
+                        Square squareObj = new Square(game, HEIGHT, WIDTH, false);
+                    }
+                }
+            }
         }
     }
 }
@@ -51,6 +66,7 @@ public class Game {
 public class Player {
 
     public boolean lost = false;
+
     public Player(String name, int playerNumber) {
         this.name = name;
         this.playerNumber = playerNumber;
