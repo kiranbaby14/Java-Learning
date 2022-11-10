f6c5e755020c0afbe635904466b39759341ca53e
kiranbaby14 <kiranbaby256@gmail.com>
2022-11-10 11:55:59 +0000

    update coursework3

--- CS5001-p3-chatserver/src/IrcServerMain.java
+++ CS5001-p3-chatserver/src/IrcServerMain.java
@@ -231,10 +231,11 @@ public class IrcServerMain implements Runnable {
                             if (!channelExists) {
                                 this.out.println(":" + serverName + " 400 * :No channel exists with that name");
                             } else {
-                                this.out.println(":" + serverName + " 353 " + this.nickName + " = " + channelName + " :");
+                                this.out.print(":" + serverName + " 353 " + this.nickName + " = " + channelName + " :");
                                 for (String names : channelNames) {
                                     this.out.print(names);
                                 }
+                                this.out.println();
                             }
 
                         } else {
